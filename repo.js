67f238e90d27eb630e0991eb9ab7ee8d4543f3e0
Tomas Estrada Ruiz <tomasestradaruiz07@gmail.com>
2023-07-28 11:09:40 -0500

    App3

--- repo.js
+++ repo.js
@@ -1,3 +1,5 @@
-let numero1 = prompt("Ingrese el primer número:")
-let numero2 = prompt("Ingrese el segundo número")
+let numero1 = parseFloat(prompt("Ingrese el primer número:"))
+let numero2 = parseFloat(prompt("Ingrese el segundo número"))
 
+let promedio = (numero1 + numero2) /2
+alert("El promedio de los numero es: " + promedio) 
